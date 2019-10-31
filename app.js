657c31e2af30d999465422a366f117342974054c
Robert Neal <robneal28@gmail.com>
2019-10-31 18:08:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
     
 //mongoose.connect("mongodb://localhost/yelp_camp_v11");
-mongoose.connect("mongodb://robert:password28@ds135592.mlab.com:35592/robneal-yelpcamp");
+mongoose.connect("mongodb://robert:<password>@ds135592.mlab.com:35592/robneal-yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
